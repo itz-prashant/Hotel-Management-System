a236d9afee9f6f6316b80625df2caae96b50397e
Prashant gupta <prashantguptaofficial007@gmail.com>
2024-05-20 12:28:10 +0530

    switch Tabs & Table html css completed

--- js/profile.js
+++ js/profile.js
@@ -13,3 +13,26 @@ logoutBtn.addEventListener("click",()=>{
     sessionStorage.removeItem("__au__")
     window.location = "index.html"
 })
+
+// ======= Tabs ======= 
+const tabs = {
+    bookingTab : document.getElementById('booking'),
+    inhouseTab : document.getElementById('in-house'),
+    archiveTab : document.getElementById('archive'),
+    cashierTab : document.getElementById('cashier')
+}
+
+const handleTabclick = (activeTab)=>{
+    Object.keys(tabs).forEach(tab =>{
+        if(tab === activeTab){
+            tabs[tab].classList.add("bg-blue-500")
+        }
+        else{
+            tabs[tab].classList.remove("bg-blue-500")
+        }
+    })
+}
+
+Object.keys(tabs).forEach(tab=>{
+    tabs[tab].addEventListener("click",()=> handleTabclick(tab))
+})
